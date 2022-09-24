ef5958104318dfc646b92f8bbe08a8ffae16c60f
Yeonjun Ko <yeonjunko@gmail.com>
2022-09-24 02:50:35 -0700

    2nd commit practice on all 3 files

--- ex2-whileloop.java
+++ ex2-whileloop.java
@@ -2,6 +2,11 @@ package com.example.main;
 
 public class Main {
     public static void main(String[] args) {
+
+
         System.out.println("1st commit practice");
+
+        System.out.println("2nd commit practice");
+
     }
 }
